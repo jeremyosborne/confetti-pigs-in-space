de2b99597e3c502964e9b407754dfdf18c641279
JO <jeremywosborne@gmail.com>
2023-12-22 09:07:42 -0800

    WIP...

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ import { Game } from "phaser";
 
 var game = new Game({
     backgroundColor: "#000000",
+
     // Idiomatic "full browser" sizing in Phaser3.
     // TODO: this will window resize listener to reset when the window changes.
     width: window.innerWidth,
     height: window.innerHeight,
-    // What element do we want to use as the parent.
-    // document.querySelector(".game-container"),
+
+    // Selects by element `id`.
     parent: "game-container",
-    // Let Phaser choose the renderer.
     type: Phaser.AUTO,
 });
