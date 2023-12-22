a5adfdcf5a497acb9ba04ae39ae67df4dfc00f45
JO <jeremywosborne@gmail.com>
2023-12-22 07:53:25 -0800

    more WIP...

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import phaser from "phaser";
+import Phaser from "phaser";
 
-console.log("WIP...");
+var game = new Phaser.Game({
+    // Let Phaser choose the renderer.
+    type: Phaser.AUTO,
+    // String dimensions are considered percentages of parent container.
+    width: "100",
+    height: "100",
+    // What element do we want to use as the parent.
+    // document.querySelector(".game-container"),
+});
