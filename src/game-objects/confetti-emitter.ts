7e6e3b19724bf5fe9585e1d12a5ba5f1f230af6f
JO <jeremywosborne@gmail.com>
2023-12-26 08:50:24 -0800

    think I figured out the 3.70 emitter

--- src/game-objects/confetti-emitter.ts
+++ src/game-objects/confetti-emitter.ts
@@ -1,26 +1,46 @@
 import { Display, GameObjects, Scene } from "phaser";
 
+/**
+ * A reusable, general particle emitter we use for explosions.
+ */
 export class ConfettiEmitter extends GameObjects.Particles.ParticleEmitter {
-    constructor(scene: Scene) {
-        const quantity = 10;
+    /** Default number of particles. */
+    quantity: number;
+
+    constructor(scene: Scene, quantity = 15) {
         super(scene, 0, 0, "confetti", {
-            blendMode: "ADD",
-            emitting: true,
+            // blendMode: "ADD",
+            // Whether this emitter constantly emits or not.
+            emitting: false,
+            gravityX: 0,
             gravityY: 0,
             lifespan: 2000,
+            // maxParticles: quantity,
             quantity,
-            speed: 100,
+            rotate: { start: 0, end: 720 },
+            // Prefer fade out, but I can't figure that DSL out at the moment (probably a tween).
+            scale: { start: 1.0, end: 0 },
+            speed: 90,
+            // stopAfter: quantity,
+            // `color` and `tint` are mutually exclusive.
+            // According to docs, tint should not include alpha channel, just
+            // hex color, like `0xff0000`.
             tint: Array.apply(null, new Array(quantity)).map(() => {
                 return Display.Color.RandomRGB().color;
             }),
+            // (true) override the color vs. (false) additive blended tinting
+            tintFill: true,
         });
+        // Inversion of control, since the scene needs to know about this for it to work
+        // and this cannot work outside of a single scene.
         scene.add.existing(this);
+
+        // Store for later.
+        this.quantity = 15;
     }
 
     spawn(x: number, y: number) {
-        this.setPosition(x, y);
-        this.setVisible(true);
-        this.emitting = true;
-        this.explode();
+        // This seems better than explode or anything else.
+        this.emitParticleAt(x, y, this.quantity);
     }
 }
