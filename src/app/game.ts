359c376023f7202f5a77645eb7b1954550d4a452
JO <jeremywosborne@gmail.com>
2024-01-09 09:26:43 -0800

    Basic loader.

---
+++ src/app/game.ts
@@ -0,0 +1,29 @@
+//
+// Code making use of Phase 3.70.0
+// This code must be transpiled before being run.
+//
+
+import { config } from "./config";
+import { Game } from "phaser";
+import { End, Play, Title } from "./scenes";
+
+export const main = () => {
+    new Game({
+        // Idiomatic "full browser" sizing in Phaser3.
+        width: window.innerWidth,
+        height: window.innerHeight,
+
+        backgroundColor: "#000000",
+
+        // Selects by element `id`.
+        parent: "game-container",
+        physics: {
+            default: "arcade",
+            arcade: {
+                debug: config().PHASER_PHYSICS_ARCADE_DEBUG,
+            },
+        },
+        scene: [Title, Play, End],
+        type: Phaser.AUTO,
+    });
+};
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,27 +1,11 @@
-//
-// Code making use of Phase 3.70.0
-// This code must be transpiled before being run.
-//
+document.addEventListener("DOMContentLoaded", () => {
+    const loadingContainer = document.getElementById("loading");
+    const gameContainer = document.getElementById("game-container");
 
-import { config } from "./config";
-import { Game } from "phaser";
-import { End, Play, Title } from "./scenes";
-
-new Game({
-    // Idiomatic "full browser" sizing in Phaser3.
-    width: window.innerWidth,
-    height: window.innerHeight,
-
-    backgroundColor: "#000000",
-
-    // Selects by element `id`.
-    parent: "game-container",
-    physics: {
-        default: "arcade",
-        arcade: {
-            debug: config().PHASER_PHYSICS_ARCADE_DEBUG,
-        },
-    },
-    scene: [Title, Play, End],
-    type: Phaser.AUTO,
+    // Dynamic import of your Phaser game
+    import("./game").then((game) => {
+        loadingContainer.classList.add("hidden");
+        gameContainer.classList.remove("hidden");
+        game.main(); // Function to initialize and start your game
+    });
 });
