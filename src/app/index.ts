3f9fa4d9e097fa533a7becc5c40b49ecd41fc3d1
JO <jeremywosborne@gmail.com>
2024-01-11 08:28:31 -0800

    loading screen tweaks.

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,15 @@
+//
+// Script loading
+//
+// Control loading screen, which we assume is present in the HTML,
+// and turn off when our (quite large) game script has loaded.
+//
 document.addEventListener("DOMContentLoaded", () => {
-    const loadingContainer = document.getElementById("loading");
+    const loadingContainer = document.getElementById("pk-bios-loading");
     const gameContainer = document.getElementById("game-container");
 
-    // Dynamic import of your Phaser game
     import("./game").then((game) => {
-        loadingContainer.classList.add("hidden");
+        loadingContainer.classList.add("loaded");
         gameContainer.classList.remove("hidden");
         game.main(); // Function to initialize and start your game
     });
